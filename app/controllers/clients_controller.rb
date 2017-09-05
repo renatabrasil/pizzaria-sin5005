54eb840bfbd16ad6103169379e1855140024270b
Renata Brasil <renatabbss@gmail.com>
2017-09-05 13:52:05 -0300

    Configuracao do Devise e criacao do CRUD Cliente

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,52 @@
 class ClientsController < ApplicationController
-  def new
-  end
 
-  def create
-  end
+	def index
+		@clients = Client.all
+	end
+
+	def new
+		@client = Client.new
+	end
+
+	def edit
+	  @client = Client.find(params[:id])
+	end
+
+	def create
+  		@client = Client.new(client_params)
+
+	  	if @client.save
+		    redirect_to @client
+	  	else
+		    render 'new'
+		end
+
+	end
+
+	def update
+	  @client = Client.find(params[:id])
+	 
+	  if @client.update(client_params)
+	    redirect_to @client
+	  else
+	    render 'edit'
+	  end
+	end
+
+
+	def show
+  		@client = Client.find(params[:id])
+	end
+
+	def destroy
+	    @client = Client.find(params[:id])
+	    @client.destroy
+	 
+	    redirect_to clients_path
+	  end
+
+	private
+		def client_params
+			params.require(:client).permit(:name, :phone, :address)
+		end
 end
---
+++ db/migrate/20170905160649_add_columns_to_clients.rb
@@ -0,0 +1,4 @@
+class AddColumnsToClients < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
