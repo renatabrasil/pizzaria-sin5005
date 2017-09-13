dead1168635177501fa014fbb7fec92c175f9666
Renata Brasil <renatabbss@gmail.com>
2017-09-13 10:45:18 -0300

    Correcao do arquivo travis.yml para rodar as migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906211117) do
+ActiveRecord::Schema.define(version: 20170906213202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20170906211117) do
   create_table "pizzas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "price", null: false
   end
 
   create_table "sales", force: :cascade do |t|
