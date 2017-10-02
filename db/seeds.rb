13302e0b5faa80fd647a5e53689704999a2d3015
Renata Brasil <renatabbss@gmail.com>
2017-10-02 11:38:21 -0300

    Adicao do usuario admin no sistema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create! :username => 'admin', :email => 'admin@gmail.com',
+  :password => 'admin123', :password_confirmation => 'admin123'
