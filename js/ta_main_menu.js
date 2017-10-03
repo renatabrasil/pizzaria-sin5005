b3269be347e736785926d33f70529bcc3ced5ec9
Rafael <rafmascarenhas@hotmail.com>
2017-10-03 13:36:20 -0300

    Templates

---
+++ js/ta_main_menu.js
@@ -0,0 +1,10 @@
+/*
+$("li").on("click", function(){
+  $("li").removeClass("active");
+  $("li").css("font-weight","");
+  $(this).addClass("active");
+  $(this).css("font-weight","bold");
+  var arq = $(this).attr("id")+".php";
+  $("#steps").load(arq);
+});
+*/
---
+++ logado.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Rafael Coxinha</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/topalert.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/bootbox.min.js"></script>
+
+</head>
+<body>
+
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" style="color:#ffffff;">Pizzaria USP</a>
+    </div>
+    <ul class="nav navbar-nav">
+
+
+ 	<li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Funcion&aacute;rios<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Novo</a></li>
+                    <li><a href="#">Editar</a></li>
+                    <li><a href="#">Consultar</a></li>
+                </ul>
+        </li>
+
+ 	<li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cliente<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Novo</a></li>
+                    <li><a href="#">Editar</a></li>
+                    <li><a href="#">Consultar</a></li>
+                </ul>
+        </li>
+
+ 	<li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pizza<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Novo</a></li>
+                    <li><a href="#">Editar</a></li>
+                    <li><a href="#">Consultar</a></li>
+                </ul>
+        </li>
+
+ 	<li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pedido<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Novo</a></li>
+                    <li><a href="#">Editar</a></li>
+                    <li><a href="#">Consultar</a></li>
+                </ul>
+        </li>
+
+    </ul>
+  </div>
+</nav>
+<script src="js/ta_main_menu.js"></script>
+<div id="steps"></div>
+
+
