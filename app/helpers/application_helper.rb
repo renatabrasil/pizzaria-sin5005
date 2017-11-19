277858195013dcc64c93762c5b5e56439226c648
Renata Brasil <renatabbss@gmail.com>
2017-11-19 20:55:17 -0200

    Atualiza o caso de uso de Fazer Pedido
    - Adiciona os arquivos de fonte para ícones
    - Adiciona o layout de pedido com opção de impressão
    - Atualiza outras páginas de detalhes de CRUD.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def full_date(date)
+    # formatting date: Aug, 31 2007 - 9:55PM
+    date.strftime("%d/%m/%Y - %H:%M")
+  end
+
+  def full_date_alt(date)
+    # formatting date: Aug, 31 2007 - 9:55PM
+    date.strftime("%d/%m/%Y (%H:%M:%S)")
+  end
+
+  def date(date)
+    # formatting date: Aug, 31 2007 - 9:55PM
+    date.strftime("%d/%m/%Y")
+  end
 end
