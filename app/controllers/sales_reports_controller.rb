3551d04291d912f3c1045ffa7a6db4f95740adfc
Renata Brasil <renatabbss@gmail.com>
2017-11-21 23:03:31 -0200

    Adiciona uma sessao com informacoes sobre o filtro do relatorio.

--- app/controllers/sales_reports_controller.rb
+++ app/controllers/sales_reports_controller.rb
@@ -16,21 +16,23 @@ class SalesReportsController < ApplicationController
     @sales_report.order_status = params[:sales_report][:order_status]
     @sales_report.initial_date = params[:sales_report][:initial_date]
     @sales_report.final_date = params[:sales_report][:final_date]
-    @sales_report.seller.id = params[:sales_report][:seller_id]
+    @sales_report.seller_id = params[:sales_report][:seller_id]
 
     query = ""
     if !@sales_report.order_status.blank?
       query = query + " status = '" + @sales_report.order_status + "'"
-      #  FIXME: Corrigir o formato da data. Usando o where sem a string funciona.
-    # elsif !@sales_report.initial_date.blank? && !@sales_report.final_date.blank?
+    end
+    #  FIXME: Corrigir o formato da data. Usando o where sem a string funciona.
+    # if !@sales_report.initial_date.blank? && !@sales_report.final_date.blank?
     #   query = query + " created_at BETWEEN " + @sales_report.initial_date +
     #     " AND " + @sales_report.final_date
     #   elsif !@sales_report.initial_date.blank?
     #     query = query + " created_at = " + @sales_report.initial_date
     #   elsif !@sales_report.final_date.blank?
     #     query = query + " created_at = " + @sales_report.final_date
-    elsif !@sales_report.seller.id.blank?
-      query = query + " employee_id = " + @sales_report.seller.id.to_s
+    # end
+    if !@sales_report.seller_id.blank?
+      query = query + " AND employee_id = " + @sales_report.seller_id.to_s
     end
     puts "query " + query
 
