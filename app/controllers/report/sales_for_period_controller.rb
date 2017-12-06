04942875ccac916ff39b74053ec3f1f72316abdb
Renata Brasil <renatabbss@gmail.com>
2017-12-06 00:25:00 -0200

    Adiciona a classificação por meses no relatorio de vendas por periodo

--- app/controllers/report/sales_for_period_controller.rb
+++ app/controllers/report/sales_for_period_controller.rb
@@ -13,36 +13,62 @@ class Report::SalesForPeriodController < ApplicationController
   end
 
   def index
-  # @sales_report = SalesReport.new(params[:sales_report])
-  @sales_report = Report::Sales.new
-  @sales_report.month = params[:sales_report][:month]
-  @sales_report.year = params[:sales_report][:year]
+    # @sales_report = SalesReport.new(params[:sales_report])
+    @sales_report = Report::Sales.new
+    @sales_report.month = params[:sales_report][:month]
+    @sales_report.year = params[:sales_report][:year]
 
-  @sales = []
+    @sales = []
 
-  # TODO: Otimizar. Pode ser que seja melhor executar uma consulta só.
-  if !@sales_report.year.blank?
-    @sales = Order.where("extract(year from created_at) = ?", @sales_report.year)
-    puts "\nAno: " + @sales_report.year
-  end
-
-  if !@sales_report.month.blank?
-    @sales = Order.where("extract(month from created_at) = ?", @sales_report.month)
-    puts "\nMonth: " + @sales_report.month
-  end
-
-
-  @total_sales = 0
-  unless @sales.empty?
-    @sales.each do |order|
-      @total_sales = @total_sales + order.value
+    # TODO: Otimizar. Pode ser que seja melhor executar uma consulta só.
+    if !@sales_report.year.blank?
+      @sales = Order.where("extract(year from created_at) = ?", @sales_report.year)
+      puts "\nAno: " + @sales_report.year
     end
+
+    if !@sales_report.month.blank?
+      @sales = Order.where("extract(month from created_at) = ?", @sales_report.month)
+      puts "\nMonth: " + @sales_report.month
+    end
+
+    @sales = classify(@sales)
+
+    @total_sales = 0
+    unless @sales.empty?
+      @sales.each do |month, sales|
+        @total_sales = @total_sales + @sales[month][1]
+      end
+    end
+
+    puts @total_sales
+
+    # @sales = Order.where(status: @sales_report.order_status, created_at: @sales_report.initial_date..@sales_report.final_date,
+    #   employee_id: @sales_report.seller.id)
+
+    render 'new'
   end
 
-  # @sales = Order.where(status: @sales_report.order_status, created_at: @sales_report.initial_date..@sales_report.final_date,
-  #   employee_id: @sales_report.seller.id)
+  private
+    def classify(result)
+      classified_result = Hash.new
+      month = -1
+      total_value = 0
+      result.each do |order|
+        if (month < order.created_at.month)
+          if month > -1
+            classified_result[month][1] = total_value
+            total_value = 0
+          end
 
-  render 'new'
-end
+          month = order.created_at.month
+          classified_result[month] = [[], total_value]
+        end
+        total_value = total_value + order.value
+        classified_result[month][0].push(order)
+      end
+      classified_result[month][1] = total_value
+      puts classified_result.inspect
+      return classified_result
+    end
 
 end
